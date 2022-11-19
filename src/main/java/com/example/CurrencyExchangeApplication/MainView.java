fd1da5c2db8c09c8c624e5b645082042436530f3
Rydzanicz <62994052+Rydzanicz@users.noreply.github.com>
2022-11-19 19:10:51 +0100

    Fixed showCalculator

--- src/main/java/com/example/CurrencyExchangeApplication/MainView.java
+++ src/main/java/com/example/CurrencyExchangeApplication/MainView.java
@@ -3,8 +3,9 @@ package com.example.CurrencyExchangeApplication;
 import com.example.CurrencyExchangeApplication.NbpClient.Currency.Currency;
 import com.example.CurrencyExchangeApplication.NbpClient.Currency.CurrencyRate;
 import com.example.CurrencyExchangeApplication.NbpClient.NBPClient;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 import java.io.IOException;
@@ -23,13 +24,17 @@ import java.time.LocalDate;
 
     private void showCalculator() {
         CurrencyRate currencyRate = new CurrencyRate(Currency.USD);
+        TextField value = new TextField("Ilość");
 
         try {
             currencyRate = clientNBP.getCurrencyExchangeRateBuyAndSell(currencyRate);
         } catch (IOException e) {
             System.out.println("Nie udało się pobrać dane");
         }
-        add(new H1(currencyRate.toString()));
+        add(new Span("Waluta: "));
+        add(new Span(currencyRate.getCurrency().toString()));
+        add(new Hr());
+        add(String.valueOf(value));
 
     }
 }
